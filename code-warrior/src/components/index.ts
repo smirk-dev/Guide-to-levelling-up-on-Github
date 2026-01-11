c9f2906f171407047a5817f445b2db8699c2e6ad
smirk-dev <suryansh.mishra.sm6@gmail.com>
2026-01-11 22:11:37 +0530

    feat: add index file to export components for better organization and accessibility

---
+++ code-warrior/src/components/index.ts
@@ -0,0 +1,24 @@
+// Icons
+export * from './icons/PixelIcons';
+
+// UI Components
+export * from './ui/PixelComponents';
+
+// RPG Components
+export { CharacterSheet } from './rpg/CharacterSheet';
+export { QuestCard, QuestLog } from './rpg/QuestCard';
+export { BadgeSlot, BadgeGrid } from './rpg/BadgeSlot';
+export { LeaderboardCard, LeaderboardTable } from './rpg/LeaderboardCard';
+
+// Layout Components
+export { Sidebar, Header, PageLayout } from './layout/Navigation';
+
+// Effects
+export {
+  FloatingXP,
+  Confetti,
+  RankUpModal,
+  QuestCompleteModal,
+  LoadingScreen,
+  Toast,
+} from './effects/Effects';
