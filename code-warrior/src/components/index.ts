734eecf92702604fa3e92d775e84c5ecd0ede01c
smirk-dev <suryansh.mishra.sm6@gmail.com>
2026-01-13 17:32:19 +0530

    feat: export additional RPG components for enhanced functionality

--- code-warrior/src/components/index.ts
+++ code-warrior/src/components/index.ts
@@ -9,6 +9,10 @@ export { CharacterSheet } from './rpg/CharacterSheet';
 export { QuestCard, QuestLog } from './rpg/QuestCard';
 export { BadgeSlot, BadgeGrid } from './rpg/BadgeSlot';
 export { LeaderboardCard, LeaderboardTable } from './rpg/LeaderboardCard';
+export { GameHUD } from './rpg/GameHUD';
+export { BattleStatsPanel } from './rpg/BattleStatsPanel';
+export { ActivityHeatmap } from './rpg/ActivityHeatmap';
+export { AchievementBadges, AchievementBadgesCompact } from './rpg/AchievementBadges';
 
 // Layout Components
 export { Sidebar, Header, PageLayout } from './layout/Navigation';
