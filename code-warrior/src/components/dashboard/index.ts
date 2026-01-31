82285bb4690edc25c2b75837dfc8ed809acbe4f5
smirk-dev <suryansh.mishra.sm6@gmail.com>
2026-01-31 19:32:02 +0530

    feat: add index.ts for exporting dashboard components

---
+++ code-warrior/src/components/dashboard/index.ts
@@ -0,0 +1,6 @@
+// Dashboard Components
+export { TabNavigation, type DashboardTab } from './TabNavigation';
+export { HeroSidebar } from './HeroSidebar';
+export { QuickViewTab } from './QuickViewTab';
+export { ActivityViewTab } from './ActivityViewTab';
+export { QuestsViewTab } from './QuestsViewTab';
